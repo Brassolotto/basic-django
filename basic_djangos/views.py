4ca68113784373904506dd200549e9bff118db5e
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-02-25 17:13:41 -0300

    Add URL routing and index view for basic_djangos app

--- basic_djangos/views.py
+++ basic_djangos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Página principal de Basic Django."""
+    return render(request, 'basic_djangos/index.html')
