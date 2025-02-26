81849f02894d73437bb8d11b95a0f527c75a609b
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-02-26 16:06:01 -0300

    Add topics view and template with URL routing for basic_djangos app

--- basic_djangos/views.py
+++ basic_djangos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Página principal de Basic Django."""
     return render(request, 'basic_djangos/index.html')
+
+def topics(request):
+    """Página de tópicos de Basic Django."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'basic_djangos/topics.html', context)
